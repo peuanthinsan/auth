e146577146b94da3d09e50d0749e769b32fe388e
peuanthinsan <heartpickle@gmail.com>
2025-07-03 01:37:36 +0700

    Update font and text color

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -2,12 +2,17 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   typography: {
-    fontFamily: `'Poppins', sans-serif`,
+    fontFamily: `'Inter', sans-serif`,
     h1: { fontSize: '2rem', fontWeight: 500 },
     h2: { fontSize: '1.5rem', fontWeight: 500 },
     h3: { fontSize: '1.25rem', fontWeight: 500 },
     h4: { fontSize: '1.125rem', fontWeight: 500 },
     body1: { fontSize: '1rem' }
+  },
+  palette: {
+    text: {
+      primary: '#333'
+    }
   },
   spacing: 8
 });
